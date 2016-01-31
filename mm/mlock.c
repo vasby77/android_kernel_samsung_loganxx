2bdd69e944ade5a25b837d73d7f692e27f64a1d7
ZIM555 <zim555@mail.ru>
2016-01-31 14:56:26 +0200

    UPSTREAM: mm: reorder can_do_mlock to fix audit denial A userspace call to mmap(MAP_LOCKED) may result in the successful locking of memory while also producing a confusing audit log denial.  can_do_mlock checks capable and rlimit.  If either of these return positive can_do_mlock returns true.  The capable check leads to an LSM hook used by apparmour and selinux which produce the audit denial.  Reordering so rlimit is checked first eliminates the denial on success, only recording a denial when the lock is unsuccessful as a result of the denial.

--- mm/mlock.c
+++ mm/mlock.c
@@ -23,10 +23,10 @@
 
 int can_do_mlock(void)
 {
-	if (capable(CAP_IPC_LOCK))
-		return 1;
 	if (rlimit(RLIMIT_MEMLOCK) != 0)
 		return 1;
+	if (capable(CAP_IPC_LOCK))
+ 		return 1;
 	return 0;
 }
 EXPORT_SYMBOL(can_do_mlock);
